07497b5219bcc8d1bcd8d581e8506b2dc20a1da9
Doctor Manhattan <m1@apple.com>
2021-06-17 16:02:56 +0800

    upgrade flutter v2

--- android/app/src/main/java/dev/mypub/flutter_flip_clock/MainActivity.java
+++ android/app/src/main/java/dev/mypub/flutter_flip_clock/MainActivity.java
@@ -1,13 +1,15 @@
 package dev.mypub.flutter_flip_clock;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
   }
 }
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,10 +2,11 @@
 // Generated file. Do not edit.
 //
 
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:wakelock_web/wakelock_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
